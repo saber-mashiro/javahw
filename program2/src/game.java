1026c9b011c9dff4a087dc11929dba20618d558b
saber-mashiro <1245450557@qq.com>
2020-11-25 09:28:26 +0800

    finishblank

--- program2/src/game.java
+++ program2/src/game.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.awt.*;
 import java.lang.*;
+import java.util.Scanner;
+import java.util.Vector;
 
 public class game {
     public static void main(String[] args) {
@@ -33,7 +35,7 @@ public class game {
         // }
         // }
         // }
-        FileReader file = null; 
+        FileReader file = null;
         try {
             file = new FileReader("word.txt");
         } catch (FileNotFoundException e) {
@@ -43,6 +45,8 @@ public class game {
     }
 
     public static void txtString(FileReader file) {
+        // Vector v = new Vector<>();
+
         BufferedReader br = new BufferedReader(file);
         try {
             String line;
@@ -52,10 +56,57 @@ public class game {
                 str[count] = line;
                 count++;
             }
-            System.out.println(str[1]);
+            int tmp = position(2100);
+            System.out.println(str[tmp]);
+            // v.resize(str[tmp].size());
+            // System.out.println(v[0]);
+            char[] v = str[tmp].toCharArray();
+            int tmp2 = position2(str[tmp].length());
+            // char c1 = v[tmp2];
+
+            int tmp3 = position2(str[tmp].length());
+            while (tmp3 == tmp2) {
+                tmp3 = position2(str[tmp].length());
+            }
+            System.out.println(str[tmp + 1]);
+            char c1, c2;
+            if (tmp2 < tmp3) {
+                c1 = v[tmp2];
+                c2 = v[tmp3];
+                System.out.println(tmp2 + "  " + tmp3);
+            } else {
+                c1 = v[tmp3];
+                c2 = v[tmp2];
+                System.out.println(tmp3 + "  " + tmp2);
+            }
+            // char c2 = v[tmp3];
+            
+            // System.out.println(tmp2 + " " + tmp3);
+            Scanner scanner = new Scanner(System.in);
+            char cha1 = scanner.next().charAt(0);
+            char cha2 = scanner.next().charAt(0);
+            if (cha1 == c1 && cha2 == c2) {
+                System.out.println("right");
+            }
         } catch (IOException e) {
             e.printStackTrace();
-            //adjfkald akjdlf
+            // adjfkald akjdlf
         }
     }
+
+    public static int position(int k) {
+        int num = (int) (Math.random() * k);
+        if (num % 2 == 0) {
+            return num;
+        } else {
+            return num + 1;
+        }
+
+    }
+
+    public static int position2(int k) {
+        int num = (int) (Math.random() * k);
+        return num;
+    }
+
 }
