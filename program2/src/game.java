e83e72881d8b80d46e07ec39588b6fe65a9a1b27
saber-mashiro <1245450557@qq.com>
2020-11-25 14:55:31 +0800

    half

--- program2/src/game.java
+++ program2/src/game.java
@@ -6,35 +6,6 @@ import java.util.Vector;
 
 public class game {
     public static void main(String[] args) {
-        // readword();
-        // }
-        //
-
-        // public static void readword(){
-        // FileReader reader = null;//??????????
-        // try{
-        // reader = new FileReader("word.txt");//??????��?
-        // char [] buffer = new char [2000];//???????��
-        // StringBuffer builder = new StringBuffer();
-        // int count;
-        // while((count = reader.read(buffer))!= -1)
-        // {
-        // builder.append(buffer,0,count);//count???????????????stringbuffer.append?buffer???????????????
-        // // System.out.println(buffer);
-        // }
-        // System.out.println(builder.toString()); //tostring ??????????????
-        // }catch(IOException e){
-        // e.printStackTrace();
-        // }finally{
-        // if (reader != null) {
-        // try {
-        // reader.close();
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
-        // }
-        // }
-        // }
         FileReader file = null;
         try {
             file = new FileReader("word.txt");
@@ -45,7 +16,6 @@ public class game {
     }
 
     public static void txtString(FileReader file) {
-        // Vector v = new Vector<>();
 
         BufferedReader br = new BufferedReader(file);
         try {
@@ -57,7 +27,7 @@ public class game {
                 count++;
             }
             int tmp = position(2100);
-            System.out.println(str[tmp]);
+            //System.out.println(str[tmp]);
             // v.resize(str[tmp].size());
             // System.out.println(v[0]);
             char[] v = str[tmp].toCharArray();
@@ -67,31 +37,38 @@ public class game {
             int tmp3 = position2(str[tmp].length());
             while (tmp3 == tmp2) {
                 tmp3 = position2(str[tmp].length());
-            }
-            System.out.println(str[tmp + 1]);
+            }            
             char c1, c2;
+            String result ;
+            result = str[tmp];
             if (tmp2 < tmp3) {
                 c1 = v[tmp2];
                 c2 = v[tmp3];
-                System.out.println(tmp2 + "  " + tmp3);
+                //System.out.println(tmp2 + "  " + tmp3);
             } else {
                 c1 = v[tmp3];
                 c2 = v[tmp2];
-                System.out.println(tmp3 + "  " + tmp2);
+                //System.out.println(tmp3 + "  " + tmp2);
             }
-            // char c2 = v[tmp3];
-            
-            // System.out.println(tmp2 + " " + tmp3);
+            v[tmp2] = '_';
+            v[tmp3] = '_';
+            for(int i = 0;i<str[tmp].length();i++)
+            {
+                System.out.print(v[i]);
+            }
+            System.out.print("\n");
+            System.out.println(str[tmp + 1]);
             Scanner scanner = new Scanner(System.in);
             char cha1 = scanner.next().charAt(0);
             char cha2 = scanner.next().charAt(0);
             if (cha1 == c1 && cha2 == c2) {
-                System.out.println("right");
+                System.out.println("right +1");
+                System.out.println(result);
             }
         } catch (IOException e) {
             e.printStackTrace();
-            // adjfkald akjdlf
         }
+        
     }
 
     public static int position(int k) {
